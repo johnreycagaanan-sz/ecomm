d302289c7e2e481ecef5a960b71d46a1c6b87069
unknown <john.cagaanan@supportzebra.com>
2022-11-01 11:05:03 +0800

    fixed error

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,4 @@
-const Category = require('../models/category');
+const Category = require('../models/Category');
 const getCategories = async (req, res, next) => {
 
     if(Object.keys(req.query).length){
