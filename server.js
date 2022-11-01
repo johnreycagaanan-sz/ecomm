c9bdd1c5863d3e7c9b6adf37220d9b2419f90c96
unknown <john.cagaanan@supportzebra.com>
2022-11-01 10:44:30 +0800

    final version

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const user = require('./routes/user');
 const item = require('./routes/item');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
-const fileupload = require('express-fileupload')
+const fileupload = require('express-fileupload');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
 dotenv.config({ path: './config/config.env' });
 connectDB();
@@ -25,6 +31,29 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use(fileupload());
+
+//sanitize nosql injections
+app.use(mongoSanitize());
+
+//protect from cross site scripting
+app.use(xss());
+
+//protect from http parameter pollution
+app.use(hpp());
+
+//setup rate limiter
+const limiter = rateLimit({
+    windowMs: 10* 60 *1000,
+    max:100
+})
+
+app.use(limiter);
+
+//add security headers
+app.use(helmet());
+
+//add cors protection
+app.use(cors());
 
 app.use(logger);
 
